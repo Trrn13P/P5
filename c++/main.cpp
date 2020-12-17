bb4db46bddc4cf1e49df790333db11aa105ea71a
Trrn13P <tor-andreas_93@hotmail.com>
2020-12-17 09:30:01 +0100

    last_commit 2

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,23 +2,41 @@
 
 #include <iostream>
 #include <fstream>
+//#include <string>
 using namespace std;
 
   int main(int argc, char const *argv[]) {
-    //Setting up parameters
-    int n = 10;
-    int tsteps = 100;
-    int saved_tsteps =0;
+    if (argc <= 5) {
+      cout << "Bad Usage: " << argv[0] << endl;
+      exit(1);
+    }
+    int n, tsteps, saved_tsteps;
+    float alpha;
+    string type;
+    if (argc > 1) {
+      //Setting up parameters from makefile
+      n = atoi(argv[1]);
+      tsteps = atoi(argv[2]);
+      //number of saved timesteps, 0 for saving all. 1 for saving every second
+      saved_tsteps = atoi(argv[3]);
+      alpha = atoi(argv[4]);
+      //type can be 1d or 2d
+      type = argv[5];
+    }
+
     float dx = 1./(n+1);
-    float alpha = 1./4;
     float dt = alpha*dx*dx;
 
     //Solving equation with set parameters
-    diffusion *test;
-    test = new diffusion(n ,tsteps ,saved_tsteps, dx, dt);
-    test -> backward_euler();
-    test -> forward_euler();
-    test -> crank_nicolson();
-    //test -> forward_2d();
-    delete test;
+    diffusion *solve;
+    solve = new diffusion(n ,tsteps ,saved_tsteps, dx, dt);
+    if(type=="1d"){
+      solve -> backward_euler();
+      solve -> forward_euler();
+      solve -> crank_nicolson();
+    }
+    if(type=="2d"){
+      solve -> forward_2d();
+    }
+    delete solve;
   }
